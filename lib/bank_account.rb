ddfc9d7c55d4b514c4bfb550f5637c3d673a3925
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-02 16:01:34 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,7 +3,7 @@ class BankAccount
 attr_accessor :balance, :status
 attr_reader :name
 
-def initialize(name, balance = 100, status = "open")
+def initialize(name, balance = 1000, status = "open")
   @name = name
   @balance = balance
   @status = status
