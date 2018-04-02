590d4d861533e5129d812f8b82d6aa56b651f16e
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-02 16:13:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ class Transfer
 
 attr_accessor :sender, :receiver, :status, :amount
 
-  def initialize
+  def initialize(sender)
     @sender = sender
     @receiver = receiver
     @status = "pending"
