be829f46a96dfcd48c784f152f46dfd86b45be28
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-02 16:05:17 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,9 @@
 class Transfer
   # your code here
 
-  def initialize(sender, receiver, statues = "pending", transfer_amount)
+  def initialize(sender, receiver, status = "pending", transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @transfer_amount = transfer_amount
 end
